bbdf34948116acd37d69ffb14bf31ce45ca63a2d
Ben Zelinski <bzelinski@cipherhealth.com>
2021-07-09 12:49:00 -0400

    initial commit

---
+++ test/services/mock-service/mock-service.hooks.js
@@ -0,0 +1,44 @@
+const circuitBreaker = require('../../../lib');
+
+const breakerOptions = {
+  fallback: () => {
+    console.log('FALLBACK !!!!!!!!!!!!!');
+    return {
+      fallback: 'called'
+    };
+  }
+};
+
+module.exports = {
+  before: {
+    all: [
+      circuitBreaker(breakerOptions)
+    ],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
